5f0446f850d37e52215f152a3e6cc01689dbc626
avinashpudota88 <avinashpudota02@gmail.com>
2025-11-25 01:19:18 +0530

    add comments
    Corrected the calculation of the sum and added comments.

--- calculator.py
+++ calculator.py
@@ -3,3 +3,5 @@ num1=float(input("Enter first number:"))
 num2=float(input("Enter second number:")) 
 result=num1=num2
 print(f"the sum is:{result}") 
+This is my first comment
+This is my second comment 
