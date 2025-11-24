44199c9764fcb6043e5204efe17c76c274640556
avinashpudota88 <avinashpudota02@gmail.com>
2025-11-25 01:36:47 +0530

    Refactor calculator.py by removing comments
    Removed comments and added a print statement.

--- calculator.py
+++ calculator.py
@@ -3,5 +3,4 @@ num1=float(input("Enter first number:"))
 num2=float(input("Enter second number:")) 
 result=num1=num2
 print(f"the sum is:{result}") 
-This is my first comment
-This is my second comment 
+print("Hello World") 
